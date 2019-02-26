8467e9553d7bac730b29ca975f5f19a900ea70c1
Nisha <nisha.sridhar1998@gmail.com>
2019-02-27 00:03:16 +0530

    Changed the email account

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rent.manage.app@gmail.com'
-EMAIL_HOST_PASSWORD = 'nishasridhar1998'
+EMAIL_HOST_USER = 'inframanageapp@gmail.com'
+EMAIL_HOST_PASSWORD = 'djangoproject'
 
 django_heroku.settings(locals())
